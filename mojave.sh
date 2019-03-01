c77682a41ab962b16b4d8d9ba039876c7ab55f79
Pablo <gomesp@gmail.com>
2019-03-01 23:40:13 +1300

    Update mojave.sh

--- mojave.sh
+++ mojave.sh
@@ -14,5 +14,18 @@ brew install tree
 brew install wget
 brew install yarn
 brew install node
+brew install zsh
+brew install sstp-client
 brew cask install 1password6
+brew cask install adobe-digital-editions
 brew cask install dropbox
+brew cask install calibre
+brew cask install iterm2
+brew cask install vlc
+brew cask install firefox
+brew cask install slack
+# How to make brew multi-user
+# https://medium.com/@energee/install-brew-for-multiple-users-65af2444df5c
+# create user group brew and add users to it
+# sudo chgrp -R brew $(brew --prefix)/*
+# sudo chmod -R g+w $(brew --prefix)/*
