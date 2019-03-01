93d7d3825a6053ff2a66a9533b3e651d5cfd7da1
Pablo <gomesp@gmail.com>
2019-03-01 23:27:41 +1300

    Create mojave.sh

---
+++ mojave.sh
@@ -0,0 +1,18 @@
+xcode-select --install
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# https://hackernoon.com/personal-macos-workspace-setup-adf61869cd79
+brew install mas
+brew install awscli
+brew install brew-cask-completion
+brew install cask
+brew install certbot
+brew install git
+brew install pyenv
+brew install tmux
+brew install tor
+brew install tree
+brew install wget
+brew install yarn
+brew install node
+brew cask install 1password6
+brew cask install dropbox
